012e92c21ff7cdca3a35fe832b8603858c45b407
carlos-henrique-dos-santos <carlos.hrq.rodrigues@gmail.com>
2021-10-22 20:25:19 -0300

    adiciona aplicacao

---
+++ src/test/java/com/devrodrigues/keycloakauth/KeycloakauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrodrigues.keycloakauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
